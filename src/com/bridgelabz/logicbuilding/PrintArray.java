b067812768e16e70f37d5625493e77a624691ad3
namansri01 <sri.naman.vastava@gmail.com>
2024-02-14 17:51:32 +0530

    Printing Array

---
+++ src/com/bridgelabz/logicbuilding/PrintArray.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.logicbuilding;
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Option 1: Using a for loop
+        System.out.println("Printing elements using a for loop:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+
+    }
+}
