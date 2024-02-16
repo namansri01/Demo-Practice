27cd1693b971b83e86b4291a7695511ae2b0ccdf
namansri01 <sri.naman.vastava@gmail.com>
2024-02-16 11:55:10 +0530

    [Naman] Added: Fibonacci series and Ascending order code

---
+++ src/com/bridgelabz/logicbuilding/AscendingOrder.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.logicbuilding;
+
+import java.util.Scanner;
+
+public class AscendingOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+
+        System.out.println("Sorted array in ascending order:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+}
